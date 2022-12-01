6d588b73404b939bb682b31af822f670147b8773
addinnabilal <16520342@std.stei.itb.ac.id>
2022-12-01 15:46:24 +0700

    feat: subscribe song and show subscription status on premium singer list page

--- src/api/subscription/getSubscribedSingers.php
+++ src/api/subscription/getSubscribedSingers.php
@@ -12,35 +12,44 @@
     if (!empty($_REQUEST["subscriber_id"])) {
         $subscriber_id = intval($_REQUEST["subscriber_id"]);
 
+        $sql = "SELECT creator_id, status FROM Subscription WHERE subscriber_id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $subscriber_id);
 
-        // count total number of row in song table
-        $stmt = $conn->prepare("SELECT creator_id FROM Subscription WHERE subscriber_id = ? AND status = ?");
-
-    
-        $status = "accepted";
-        $stmt->bind_param("is", $subscriber_id, $status);
     } else {
         exitWithError(500, "subscriber_id is not provided");
     }
 
-    
+    $accepted = array();
+    $pending = array();
+    $rejected = array();
+
     if ($stmt->execute()) {
         $result = $stmt->get_result();
-        $data = array();
         while ($row = $result->fetch_assoc()) {
             $creator_id = $row['creator_id'];
-            array_push($data, $creator_id);
+            if ($row['status'] == "accepted") {
+                array_push($accepted, $creator_id);
+            } else if ($row['status'] == "pending") {
+                array_push($pending, $creator_id);
+            } else if ($row['status'] == "rejected") {
+                array_push($rejected, $creator_id);
+            }
         }
-
-        $response = array(
-            "data" => $data,
-        );
           
-        $conn->close();
-        
-        exitWithDataReturned($response);
     } else {
         $conn->close();
         exitWithError(500, "Error while fetching subscribed singers");
     }
+
+    
+
+    $data = array(
+        "accepted" => $accepted,
+        "pending" => $pending,
+        "rejected" => $rejected
+    );
+
+    $conn->close();
+    exitWithDataReturned($data);
 ?>
--- src/components/premiumSingerCard/premiumSingerCard.php
+++ src/components/premiumSingerCard/premiumSingerCard.php
@@ -1,67 +1,42 @@
 <?php
 
-
-    function subscribeSinger($singer_id, $subscriber_id) {
-        $webservice_url = "http://binotify_soap:8080/api/binotify";
-
-        $request_body = '
-        <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
-            <Body>
-                <newSubscription xmlns="http://services.binotify/">
-                    <arg0 xmlns="">'. $singer_id .'</arg0>
-                    <arg1 xmlns="">'. $subscriber_id .'</arg1>
-                    <arg2 xmlns="">x1NbJ4r1e1FAyFNUColSeLG1DhXL6eUfZfDHp93/TWco1YdmDJ+U31Z1S3lLlvYBtZes1nIMwJCQ0bhTaYXkcvdlY4XJjICbedsY3eGGtWXLnZW6/VTP46kpbDQGI/EnJsTIewV8jUpFkIyR/1YD0g==</arg2>
-                </newSubscription>
-            </Body>
-        </Envelope>';
-        
-
-        $headers = array(
-            'Content-Type: text/xml; charset=utf-8');
-
-        $ch = curl_init($webservice_url);
-        curl_setopt ($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt ($ch, CURLOPT_POSTFIELDS, $request_body);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-        $result = curl_exec($ch);
-        curl_close ($ch);
-
-        if ($result !== FALSE) {
-            return $result;
-        } else {
-            return FALSE;
-        };
-    };
-
     function premiumSingerCard($args){
-        // $subscribver_id, $singer_id, $name, $is_subscribed, $on_click_subscribe, $on_click_go_to_premium_songs_page
+        // $subscribver_id, $creator_id, $singer_name, $is_subscription_accepted, $is_subscription_pending, $is_subscription_rejected, $on_click_subscribe, 
 
         extract($args);
-        echo $is_subsctibed;
         $subscribe_button = "";
+        $subscription_pending_information = "";
+        $subscription_rejected_information = "";
         $go_to_premium_songs_page_button = "";
-        if (!$is_subscribed){
-            $subscribe_button = 
+
+
+        if ($is_subscription_pending){
+            $subscription_pending_information = 
             <<<EOT
-            <button onclick="$on_click_subscribe($id)">
-                Subscribe
-            </button>
+            <p>Pending</p>
             EOT;
-            $play_button = "";
-        } else {
+        } else if ($is_subscription_accepted){
             $go_to_premium_songs_page_button = 
             <<<EOT
-            <button onclick="$on_click_go_to_premium_songs_page($id)">
-                Go To
+            <a href="../premium-song/index.php?creator_id=$creator_id&singer_name=$singer_name">
+                Song List 
+            </a>
+            EOT;
+        } else if ($is_subscription_rejected){
+            $subscription_rejected_information = 
+            <<<EOT
+            <p>Rejected</p>
+            EOT;
+        } else {
+            $subscribe_button = 
+            <<<EOT
+            <button onclick="subscribeSinger($creator_id, $subscriber_id)">
+                Subscribe
             </button>
             EOT;
         }
 
-
+        
 
         $html = <<<"EOT"
         <style>
@@ -80,21 +55,15 @@
         </style>
             <div class="flex flex-row premium-singer-card">
                     <div class="premium-singer-desc">
-                        $name
+                        $singer_name
                     </div>
                     
                     $subscribe_button
+                    $subscription_pending_information
+                    $subscription_rejected_information
                     $go_to_premium_songs_page_button
             </div>
         EOT;
-        // echo premiumSingerCard(
-        //     singer_id: 2,
-        //     subscriber_id: 2,
-        //     name: "Singer 2",
-        //     is_subscribed: false,
-        //     on_click_subscribe: "subscribeSinger",
-        //     on_click_go_to_premium_songs_page: "goToPremiumSongsPage"
-            
-        // ) ;
+        echo $html;
     }
 ?>
